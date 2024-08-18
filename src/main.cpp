fe82db4c2e517b127ba47da54cbdc7b51604c73f
albear007 <albert.q.tang@gmail.com>
2024-08-18 22:35:01 +0300

    Upped to date...

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <fmt/core.h>
 
+#include "myAdd.hpp"
+
 int main(int argc, char *argv[]) {
   fmt::print("Hello, world!\n");
+  const int x = 10;
+  const int y = 20;
+  const auto res = myAdd::myAdd(x, y);
+
+  fmt::print("result: {}\n", res);
+
   return 0;
 }
---
+++ src/myAdd.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace myAdd {
+
+template <typename T> T myAdd(T a, T b) { return a + b; }
+
+} // namespace myAdd
